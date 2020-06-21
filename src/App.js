7cb90c8c19176dd7d924bff6fb7aad0a2b2f30a5
Alvin Sun <alvinsunyixiao@gmail.com>
2020-06-21 14:08:09 -0500

    error function implementatino of init done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // bootstrap stuff
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,9 +6,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 // tensorflow stuff
 import * as tf from '@tensorflow/tfjs'
 // plotly
@@ -16,238 +18,337 @@ import Plot from 'react-plotly.js'
 // cookies
 import { Cookies } from 'react-cookie'
 
+import download from 'downloadjs'
+import { spressoBurger, spressoBurgerInput } from './Spresso.js'
+
 const cookies = new Cookies();
 
 const default_input = {
-  sim_time:       0.05,
-  num_grids:      250,
-  domain_len:     10,
-  animation_rate: 30,
+  // simulation related
+  sim_time:         0.1,
+  animate_rate:     30,
+  // data related
+  num_grids:        250,
+  domain_len:       50,
+  injection_loc:    15,
+  injection_width:  10,
+  injection_amount: 3,
+  interface_width:  1,
 };
 
-class InputNumber extends React.Component {
+class Input extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     const { name } = this.props;
     let value = cookies.get(name);
     if (value === undefined) {
       value = default_input[name];
       cookies.set(name, value);
     }
-    this.props.update(name, value);
+    if (this.props.update)
+      this.props.update(name, this.formatValue(value));
+  }
+
+  formatValue(value) {
+    return value;
   }
 
   onChange(event) {
     cookies.set(event.target.name, event.target.value);
-    if (this.props.update !== undefined) {
-      this.props.update(event.target.name, event.target.value);
+    const value = this.formatValue(event.target.value);
+    if (this.props.update) {
+      this.props.update(event.target.name, value);
     }
   }
+}
 
+class InputNumber extends Input {
   render() {
     const { hint, placeholder, name } = this.props;
     return (
       <InputGroup size="sm">
         <InputGroup.Prepend>
-          <InputGroup.Text>{hint}</InputGroup.Text>
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 500 }}
+            overlay={
+              <Tooltip>
+                { this.props.children }
+              </Tooltip>
+            }
+          >
+            <InputGroup.Text>{hint}</InputGroup.Text>
+          </OverlayTrigger>
         </InputGroup.Prepend>
-        <FormControl
+        <Form.Control
           name={ name }
           placeholder={ placeholder }
           type="number"
           value={ cookies.get(name) }
           onChange={(event) => this.onChange(event)}
+          readOnly={ this.props.readOnly }
         />
       </InputGroup>
-    );
+    )
+  }
+}
+
+class InputInt extends InputNumber {
+  formatValue(value) {
+    return parseInt(value);
+  }
+}
+
+class InputFloat extends InputNumber {
+  formatValue(value) {
+    return parseFloat(value);
   }
 }
 
 class SimUI extends React.Component {
-  state = {
-    x: [],
-    y: [],
-    ts: [0],
-    idx: 0,
-  }
-
-  calcFlux(concentration_x) {
-    const { N, dx } = this.state;
-    const c_right = concentration_x.slice([1], [N-1]);
-    const c_left = concentration_x.slice([0], [N-1]);
-    const flux = tf.sub(
-      tf.add(c_right.square(), c_left.square()).mul(0.5),
-      tf.abs(c_right.add(c_left)).mul(c_right.sub(c_left)).mul(0.5)
-    );
-    const rhs = tf.sub(flux.slice([0], [N-2]), flux.slice([1], [N-2])).div(dx);
-    const rhs_pad = rhs.pad([[1, 1]]);
-    return rhs_pad;
-  }
-
-  simulateStep() {
-    if (!this.state.run) {
-      return;
-    }
-    const { dx, concentration_tx, ts, sim_time, animation_rate } = this.state;
-    const dt = dx;
-    let concentration_x = concentration_tx[concentration_tx.length - 1];
-    let t = ts[ts.length - 1];
-    let new_t = [];
-    let new_concentration_tx = []
-    for (let i = 0; i < animation_rate; ++i) {
-      if (t > sim_time) {
-        this.setState({
-          concentration_tx: concentration_tx.concat(new_concentration_tx),
-          ts: ts.concat(new_t),
-          idx: this.state.idx + i,
-        });
-        return;
-      }
-      // RK-4
-      const k1 = tf.tidy(() => this.calcFlux(concentration_x));
-      const k2 = tf.tidy(() => this.calcFlux(concentration_x.add(k1.mul(0.5*dt))));
-      const k3 = tf.tidy(() => this.calcFlux(concentration_x.add(k2.mul(0.5*dx))));
-      const k4 = tf.tidy(() => this.calcFlux(concentration_x.add(k3.mul(dt))));
-      const flux = tf.tidy(() => k1.add(k2.mul(2)).add(k3.mul(2)).add(k4).div(6));
-      const new_concentration_x = tf.tidy(() => flux.mul(dt).add(concentration_x));
-      concentration_x = new_concentration_x;
-      t += dt;
-      new_concentration_tx.push(concentration_x);
-      new_t.push(t);
-      // release memory
-      k1.dispose();
-      k2.dispose();
-      k3.dispose();
-      k4.dispose();
-      flux.dispose();
-    }
-    // update states
-    this.setState({
-      concentration_tx: concentration_tx.concat(new_concentration_tx),
-      ts: ts.concat(new_t),
-      idx: this.state.idx + animation_rate,
-    });
-    // request animate new frame
-    requestAnimationFrame(() => this.simulateStep());
-  }
-
-  updateInit() {
-    const { num_grids, domain_len } = this.state;
-    if (!num_grids || !domain_len) {
-      return;
-    }
-    const grid_x = tf.linspace(0, domain_len, num_grids);
-    const concentration_x = tf.exp(
-      grid_x.sub(0.1 * domain_len).square().neg().div(Math.pow(0.02 * domain_len, 2)));
-    grid_x.array().then(arr => this.setState({x: arr}));
-    concentration_x.array().then(arr => this.setState({y: arr}));
-    this.setState({
-      ts: [0],
-      idx: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: undefined,
+      y: undefined,
       run: false,
-      concentration_tx: [concentration_x],
-      grid_x: grid_x,
-      L: domain_len,
-      N: num_grids,
-      dx: domain_len / (num_grids - 1),
-    });
+    }
+    this.spresso = undefined;
+  }
+
+  simulateSteps() {
+    if (!this.state.run) {
+      return ;
+    }
+    let should_continue = true;
+    for (let i = 0; i < this.spresso.input.animate_rate && should_continue; ++i) {
+      should_continue = this.spresso.simulateStep();
+    }
+    this.spresso.getCurrentConcentration().data().then(data => this.setState({y: data}));
+    // request animate new frame
+    if (should_continue) {
+      requestAnimationFrame(() => this.simulateSteps());
+    }
+    else {
+      this.setState({run: false});
+    }
   }
 
   resetHandler() {
-    this.state.concentration_tx.forEach(c => c.dispose());
-    this.updateInit();
+    const { sim_time, num_grids, domain_len, animate_rate,
+            injection_loc, injection_width, injection_amount, interface_width } = this.state;
+    if (!sim_time || !num_grids || !domain_len || !animate_rate ||
+        !injection_loc || !injection_width ||
+        !injection_amount || !interface_width) {
+      return;
+    }
+    if (this.spresso) {
+      this.spresso.reset();
+    }
+
+    this.setState({run: false});
+    const spresso_input = new spressoBurgerInput(sim_time, animate_rate, num_grids, domain_len,
+      injection_loc, injection_width, injection_amount, interface_width);
+    this.spresso = new spressoBurger(spresso_input);
+    this.spresso.grid_x.data().then(data => this.setState({x: data}));
+    this.spresso.getCurrentConcentration().data().then(data => this.setState({y: data}));
   }
 
   componentDidUpdate(prevProps, prevState) {
+    if (this.spresso) {
+      if (prevState.sim_time !== this.state.sim_time) {
+        this.spresso.input.sim_time = this.state.sim_time;
+      }
+      if (prevState.animate_rate !== this.state.animate_rate) {
+        this.spresso.input.animate_rate = this.state.animate_rate;
+      }
+    }
     if (prevState.num_grids !== this.state.num_grids ||
-        prevState.domain_len !== this.state.domain_len) {
-      this.updateInit();
+        prevState.domain_len !== this.state.domain_len ||
+        prevState.injection_loc !== this.state.injection_loc ||
+        prevState.injection_width !== this.state.injection_width ||
+        prevState.injection_amount !== this.state.injection_amount ||
+        prevState.interface_width !== this.state.interface_width) {
+      this.resetHandler();
     }
-    if (prevState.idx !== this.state.idx) {
-      this.state.concentration_tx[this.state.idx].array().then(arr => this.setState({y: arr}));
-    }
-    if (prevState.run !== this.state.run && this.state.run) {
-      this.simulateStep();
+    if (!prevState.run && this.state.run) {
+      this.simulateSteps();
     }
   }
 
   render() {
-    return (
-      <div>
-        <Button className="m-3 btn-success" onClick={() => this.setState({run: true})}>
-          Start
-        </Button>
+    const plot = (this.state.x && this.state.y) ?
+      (
+        <Plot
+          className="mt-3"
+          data={[
+            {
+              x: this.state.x,
+              y: this.state.y,
+            }
+          ]}
+          layout={{
+            title: { text: "Concentration Plot @ " + this.spresso.getCurrentTime() + "s" },
+            xaxis: { title: { text: "Domain [m]" } },
+            yaxis: {
+              title: { text: "Concentration [mole / m^3]" },
+            }
+          }}
+          divId="concentration_plot"
+        />
+      )
+      :
+      "Loading";
+    const start_pause = this.state.run ?
+      (
         <Button className="m-3 btn-warning" onClick={() => this.setState({run: false})}>
           Pause
         </Button>
-        <Button className="m-3 btn-danger" onClick={() => this.resetHandler()}>Reset</Button>
-        <Row>
+      )
+      :
+      (
+        <Button className="m-3 btn-success" onClick={() => this.setState({run: true})}>
+          Start
+        </Button>
+      );
+    return (
+      <div>
+        <Form.Row className="mb-3">
           <Col>
-            <InputNumber
+            <InputFloat
               hint="Simulation Time"
               placeholder="[s]"
               name="sim_time"
-              update={(name, value) => this.setState({[name]: parseFloat(value)})}
-            />
+              update={(name, value) => this.setState({[name]: value})}
+            >
+              Physical simulated time in [s]
+            </InputFloat>
           </Col>
           <Col>
-            <InputNumber
+            <InputInt
+              hint="Animation Rate"
+              placeholder="[steps/update]"
+              name="animate_rate"
+              update={(name, value) => this.setState({[name]: value})}
+            >
+              Update the animated graph once every this many steps of simulation.<br/>
+              <strong>Note</strong>: lower this value to obtain smoother animation but
+                                     slower simulation.
+            </InputInt>
+          </Col>
+          <Col>
+            <InputInt
               hint="# Grid Points"
               placeholder="[s]"
               name="num_grids"
-              update={(name, value) => this.setState({[name]: parseInt(value)})}
-            />
+              update={(name, value) => this.setState({[name]: value})}
+            >
+              Number of discrete grid points in the spatial domain.
+            </InputInt>
           </Col>
           <Col>
-            <InputNumber
+            <InputFloat
               hint="Domain Length"
               placeholder="[mm]"
               name="domain_len"
-              //                                                                [mm] -> [m]
-              update={(name, value) => this.setState({[name]: parseFloat(value) * 1e-3})}
-            />
+              //                                              [mm] -> [m]
+              update={(name, value) => this.setState({[name]: value * 1e-3})}
+            >
+              Domain length in [mm].
+            </InputFloat>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <Col>
+            <InputFloat
+              hint="Injection Location"
+              placeholder="[mm]"
+              name="injection_loc"
+              update={(name, value) => this.setState({[name]: value * 1e-3})}
+            >
+              Injection Location in [mm].
+            </InputFloat>
           </Col>
           <Col>
-            <InputNumber
-              hint="Animation Rate"
-              placeholder="[steps/update]"
-              name="animation_rate"
-              update={(name, value) => this.setState({[name]: parseInt(value)})}
-            />
+            <InputFloat
+              hint="Injection Width"
+              placeholder="[mm]"
+              name="injection_width"
+              update={(name, value) => this.setState({[name]: value * 1e-3})}
+            >
+              Injection Width in [mm].
+            </InputFloat>
           </Col>
+          <Col>
+            <InputFloat
+              hint="Injection Amount"
+              placeholder="[milli moles]"
+              name="injection_amount"
+              update={(name, value) => this.setState({[name]: value * 1e-3})}
+            >
+              Amount of injected substance in [milli moles].
+            </InputFloat>
+          </Col>
+          <Col>
+            <InputFloat
+              hint="Interface Width"
+              placeholder="[mm]"
+              name="interface_width"
+              update={(name, value) => this.setState({[name]: value * 1e-3})}
+              readOnly
+            >
+              Interface width in [mm].
+            </InputFloat>
+          </Col>
+        </Form.Row>
+        <Row>
+          { start_pause }
+          <Button className="m-3 btn-danger" onClick={() => this.resetHandler()}>Reset</Button>
+          <Button className="m-3 btn-info" onClick={() => {
+            const content = JSON.stringify(this.spresso.input, null, 2);
+            download(content, "config.json", "application/json");
+          }}>Save Configurations</Button>
+          <Button className="m-3 btn-info" onClick={() => {
+            const handler = async function(spresso) {
+              const concentration_tx = spresso.getAllConcentration();
+              const data = await concentration_tx.data();
+              const blob = new Blob([data]);
+              download(blob, "data.bin");
+            }
+            handler(this.spresso);
+          }}>
+            Save Data
+          </Button>
         </Row>
         <Row>
-          <Plot
-            className="mt-3"
-            data={[
-              {
-                x: this.state.x,
-                y: this.state.y,
-                line: { simplify: false },
-              }
-            ]}
-            layout={{
-              title: { text: "Concentration Plot @ " + this.state.ts[this.state.idx] + "s" },
-              xaxis: { title: { text: "Domain [m]" } },
-              yaxis: {
-                title: { text: "Concentration [unit?]" },
-              }
-            }}
-            divId="concentration_plot"
-          />
+        </Row>
+        <Row>
+          { plot }
         </Row>
       </div>
     );
   }
 }
 
-const App = () => (
-  <Container>
-    <Jumbotron className="p-4">
-      <h1 className="header">Welcome To Spresso Simulator</h1>
-    </Jumbotron>
-    <SimUI />
-  </Container>
-);
+const App = function() {
+  const [appReady, setAppReady] = useState(false);
+  if (tf.ENV.getBool('HAS_WEBGL'))
+    tf.setBackend('webgl');
+  else
+    tf.setBackend('cpu');
+  tf.ready().then(() => setAppReady(true));
+  if (!appReady)
+    return "Loading"
+  return (
+    <Container>
+      <Jumbotron className="p-4">
+        <h1 className="header">
+          Spresso <span role="img" aria-label="burger">🍔</span> Simulator
+        </h1>
+      </Jumbotron>
+      <SimUI />
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/Spresso.js
@@ -0,0 +1,101 @@
+import * as tf from '@tensorflow/tfjs'
+
+/*
+ * sim_time:      physical time in [s]
+ * num_grids:     number of grid points
+ * domain_len:    domain length in [m]
+ * animate_rate:  animation rate in [steps / animation]
+ **/
+
+export class spressoBurgerInput {
+  constructor(sim_time, animate_rate, num_grids, domain_len,
+              injection_loc, injection_width, injection_amount, interface_width) {
+    this.sim_time = sim_time;
+    this.animate_rate = animate_rate;
+    this.num_grids = num_grids;
+    this.domain_len = domain_len;
+    this.injection_loc = injection_loc;
+    this.injection_width = injection_width;
+    this.injection_amount = injection_amount;
+    this.interface_width = interface_width;
+  }
+}
+
+export class spressoBurger {
+  constructor(input) {
+    this.input = input;
+    this.dx = input.domain_len / (input.num_grids - 1);
+    this.dt = this.dx
+    this.step = 0;
+    this.grid_x = tf.linspace(0, input.domain_len, input.num_grids);
+    this.time_t = [0];
+    this.concentration_tx = [tf.tidy(() => {
+      const { injection_loc, injection_width, injection_amount, interface_width } = this.input;
+      const erf_l = this.grid_x.add(-injection_loc+injection_width/2.).div(interface_width).erf();
+      const erf_r = this.grid_x.add(-injection_loc-injection_width/2.).div(interface_width).erf();
+      const c_norm = tf.sub(erf_l, erf_r).mul(0.5);
+      const c_norm_integral = c_norm.sum().mul(this.dx);
+      const c0 = tf.div(injection_amount, c_norm_integral);
+      return c_norm.mul(c0);
+    })]
+  }
+
+  calcFlux(numeric_step=0) {
+    const N = this.input.num_grids;
+    const { dx } = this;
+    const concentration_x = this.getCurrentConcentration().add(numeric_step);
+    const c_right = concentration_x.slice([1], [N-1]);
+    const c_left = concentration_x.slice([0], [N-1]);
+    const flux = tf.sub(
+      tf.add(c_right.square(), c_left.square()).mul(0.5),
+      tf.abs(c_right.add(c_left)).mul(c_right.sub(c_left)).mul(0.5)
+    );
+    const rhs = tf.sub(flux.slice([0], [N-2]), flux.slice([1], [N-2])).div(dx);
+    const rhs_pad = rhs.pad([[1, 1]]);
+    return rhs_pad;
+  }
+
+  getCurrentTime() {
+    return this.time_t[this.time_t.length - 1];
+  }
+
+  getCurrentConcentration() {
+    return this.concentration_tx[this.concentration_tx.length - 1];
+  }
+
+  getAllConcentration() {
+    return tf.stack(this.concentration_tx);
+  }
+
+  simulateStep() {
+    if (this.getCurrentTime() >= this.input.sim_time) {
+      return false;
+    }
+
+    const { dt } = this;
+    const concentration_x = this.getCurrentConcentration();
+    const t = this.getCurrentTime();
+    const k1 = tf.tidy(() => this.calcFlux());
+    const k2 = tf.tidy(() => this.calcFlux(k1.mul(0.5*dt)));
+    const k3 = tf.tidy(() => this.calcFlux(k2.mul(0.5*dt)));
+    const k4 = tf.tidy(() => this.calcFlux(k3.mul(dt)));
+    const flux = tf.tidy(() => k1.add(k2.mul(2)).add(k3.mul(2)).add(k4).div(6));
+    const new_concentration_x = tf.tidy(() => flux.mul(dt).add(concentration_x));
+
+    this.time_t.push(t + dt);
+    this.concentration_tx.push(new_concentration_x);
+
+    k1.dispose();
+    k2.dispose();
+    k3.dispose();
+    k4.dispose();
+    flux.dispose();
+
+    return true;
+  }
+
+  reset() {
+    this.concentration_tx.forEach(c => c.dispose());
+    this.grid_x.dispose();
+  }
+}
