06c708c7c3e0f68cbca8fdc8393be792a9915e1f
Alvin Sun <alvinsunyixiao@gmail.com>
2020-08-07 11:02:01 -0500

    test errors removed

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-canvas-mock';
+
+class Worker {
+  constructor(url) {
+    this.url = url;
+    this.onmessage = (e) => {};
+    this.postMessage = (msg) => {};
+  }
+}
+
+window.URL.createObjectURL = function() {};
+window.Worker = Worker;
